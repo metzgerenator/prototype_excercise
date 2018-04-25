0be2920390867a26985af534c7ee136f69eb2b97
metzgerenator <metzgerenator@gmail.com>
2018-04-25 14:03:37 -0700

    Changed save and unsaved to emojis

--- freeza/freeza/TopEntriesViewController.swift
+++ freeza/freeza/TopEntriesViewController.swift
@@ -167,9 +167,9 @@ extension TopEntriesViewController { // UITableViewDataSource
         entryTableViewCell.entry = entry
         
         if (self.viewModel.favoriteEntries.contains{$0.title == entry.title}) {
-            entryTableViewCell.addToFavoritesButton.setTitle("Saved", for: .normal)
+            entryTableViewCell.addToFavoritesButton.setTitle("⭐️", for: .normal)
         } else {
-            entryTableViewCell.addToFavoritesButton.setTitle("Save", for: .normal)
+            entryTableViewCell.addToFavoritesButton.setTitle("➕", for: .normal)
         }
         entryTableViewCell.delegate = self
         
